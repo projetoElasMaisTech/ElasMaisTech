e6b8ddcd104f20e2dfd5efed36c26eae981d921d
Dani <fontana.daniela@outlook.com>
2022-09-11 17:48:32 -0300

    Password recovery front fixed.

--- project/settings.py
+++ project/settings.py
@@ -143,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "elasmaistech@gmail.com"
-EMAIL_HOST_PASSWORD = "qymjcuemaiptxoeq"
+EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")
 
